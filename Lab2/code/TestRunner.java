8f58337c13d947c02fc1dc09f02a6b9525c5fff2
unknown <tnew2294@gmail.com>
2016-08-10 13:03:11 +1000

    Clean up output

--- Lab2/code/TestRunner.java
+++ Lab2/code/TestRunner.java
@@ -6,12 +6,15 @@ public class TestRunner {
 
     public static void main(String[] args)
     {
+        System.out.println("\n Running test...\n");
         Result r = JUnitCore.runClasses(TestMachinery.class);
         
         for(Failure f : r.getFailures()) {
             System.out.println(f.toString());
         }
         
-        System.out.println(r.wasSuccessful());
+        System.out.println(
+                (r.wasSuccessful() ? 
+                    "All tests passed" : "All tests failed"));
     }
 }
